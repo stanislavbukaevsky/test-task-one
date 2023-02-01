54530290b2b3aab4afa6632a96380525e44ffb9c
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-02-01 13:24:52 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/testtaskone/TestTaskOneApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testtaskone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskOneApplication.class, args);
+	}
+
+}
